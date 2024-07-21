d9c03de761a93bd1da0e801af6ec4aff6a6dca2f
Bharti <sahanibharti71@gmail.com>
2024-07-21 07:27:06 +0530

    created server,frontend file to access data through axios, faced CORS error

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,47 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+
+
 app.get('/',(req,res)=>{
     res.send('server is ready')
+})
+
+//get a list of 5 jokes
+app.get('/jokes',(req,res)=>{
+    const jokes = [
+        {
+            id:1,
+            title:'1st joke',
+            content:'this is 1st joke'
+        },
+        {
+            id:2,
+            title:'2nd joke',
+            content:'this is 2nd joke'
+        },
+        {
+            id:3,
+            title:'3rd joke',
+            content:'this is 3rd joke'
+        },
+        {
+            id:4,
+            title:'4th joke',
+            content:'this is 4th joke'
+        },
+        {
+            id:5,
+            title:'5th joke',
+            content:'this is 5th joke'
+        }
+    ]
+    res.send(jokes);
+
 })
 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`)
 })
+
+
---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+
+function App() {
+  const [jokes, setJokes] = useState([])
+
+  useEffect(()=>{
+    axios.get('http://localhost:3000/jokes')
+    .then((response)=>{
+      setJokes(response.data)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  })
+
+  return (
+   <div>
+    <h1>First Full-Stack App</h1>
+    {jokes.map((joke)=>(
+      <div key = {joke.id}>
+       <h2>{joke.title}</h2> 
+       <p>{joke.content}</p>
+      </div>
+    ))}
+   </div>
+  )
+}
+
+export default App
+
+// Access to XMLHttpRequest at 'http://localhost:3000/jokes' from origin 'http://localhost:5173' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
